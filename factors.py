5d864fa359dcce2b3ea1b9c60e016df9d2830ebc
Fo <chiapolinn@gmail.com>
2014-11-11 12:44:07 -0700

    Amended logic to include self as factor

--- factors.py
+++ factors.py
@@ -5,7 +5,7 @@ user_input = raw_input("Give us a number to factorise: ")
 user_input = int(user_input)
 factor = 1
 
-while factor < user_input:
+while factor <= user_input:
 	if user_input % factor == 0:
 		print "{} is a divisor of {}".format(factor, user_input)
-	factor += factor
+	factor = factor + 1
