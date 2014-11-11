46a097874942ac66fe463333c395933042f331b7
Fo <chiapolinn@gmail.com>
2014-11-11 12:40:33 -0700

    Flow practice: factorisation

---
+++ factors.py
@@ -0,0 +1,11 @@
+from __future__ import division
+
+user_input = raw_input("Give us a number to factorise: ")
+
+user_input = int(user_input)
+factor = 1
+
+while factor < user_input:
+	if user_input % factor == 0:
+		print "{} is a divisor of {}".format(factor, user_input)
+	factor += factor
