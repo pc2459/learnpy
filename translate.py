e9ed12ba8e1878b5abd3adf54437445d3b8127e4
Fo <chiapolinn@gmail.com>
2014-11-11 12:08:07 -0700

    First block of leetspeak code

---
+++ translate.py
@@ -0,0 +1,13 @@
+#ask user for some input
+user_input = raw_input("Enter some text: ")
+
+#replace text with 13375p34k
+user_input = user_input.replace("a","4")
+user_input = user_input.replace("b","8")
+user_input = user_input.replace("e","3")
+user_input = user_input.replace("l","1")
+user_input = user_input.replace("o","0")
+user_input = user_input.replace("s","5")
+user_input = user_input.replace("t","7")
+
+print user_input
