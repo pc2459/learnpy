9b99aa969cf5bed5c70c22e33cc3891b28295c56
Fo <chiapolinn@gmail.com>
2014-11-12 15:27:11 -0700

    Fixed silly error where it was returning the state and not the capital as an answer

--- capital_city_loop.py
+++ capital_city_loop.py
@@ -12,17 +12,17 @@ capital = capitals_dict[state]
 capital = capital.lower()
 
 print "The state is {}".format(state)
-answer = raw_input("What is its capital? ")
-answer = answer.lower()
 
 while game == 1:
+	answer = raw_input("What is its capital? ")
+	answer = answer.lower()
 	if answer == capital:
 		game = 0
 		print "YAY"
 	elif answer == "exit":
 		# Return formatting
 		capital = capitals_dict[state]
-		print "The answer was {}".format(state)
+		print "The answer was {}".format(capital)
 		print "Goodbye."
 
 print "Correct!"
