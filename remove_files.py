b4f0c7bc9efaa9e80d3c17ebb387a6aa3fde7471
Fo <chiapolinn@gmail.com>
2014-11-13 10:00:15 -0700

    Coding in the logic

--- remove_files.py
+++ remove_files.py
@@ -1,4 +1,19 @@
 import os
 import glob
 
+# this script should delete any JPGs under 2KB
+
 imgpath = "/Users/Fo/Dropbox/LearnPython/repo/images/little pics"
+
+for current, sub, files in os.walk(imgpath):
+	for file in files:
+		# check to see if they are JPGs
+		if file.lower(0).endswith(".jpg"):
+			# check if they are smaller than 2KB
+			if os.path.getsize(os.path.join(current,file)) < 2000:
+				print "remove this file"
+			else:
+				print "we do nothing; it's too big"
+		else:
+			print "we do nothing; not a jpg"
+			
