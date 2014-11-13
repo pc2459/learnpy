84daf0f1148f8da9187aae61f726d1f6335e57a3
Fo <chiapolinn@gmail.com>
2014-11-12 17:34:27 -0700

    Fixed logic errors

--- catswithhats.py
+++ catswithhats.py
@@ -2,21 +2,23 @@
 cat_list = {}
 
 # append like 10 cats to a dictionary 
-for i in range (0,10):
+for i in range (1,100):
 	cat_list[i] = "off"
 
 i = 1
 
-for cat in cat_list:
-	# if you visit the cat on round N 
-	if cat % i == 0: 
-		# do the binary action of taking a hat on or off
-		if cat_list[cat] == "off":
-			cat_list[cat] = "on"
-		else:
-			cat_list[cat] = "off"
-	# increment the way you go around the rounds
+while i < 100:
+	for cat in cat_list:
+		# if you visit the cat on round N 
+		if cat % i == 0: 
+			# do the binary action of taking a hat on or off
+			if cat_list[cat] == "off":
+				cat_list[cat] = "on"
+			else:
+				cat_list[cat] = "off"
+# increment the way you go around the rounds
 	i += 1
+
 
 # print out results of cats with hats
 for cat in cat_list:
