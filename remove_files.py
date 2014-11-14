c26f55e4c82d0dda120088e5c4c3f5a7a5fa62ef
Fo <chiapolinn@gmail.com>
2014-11-13 17:27:46 -0700

    Added working remove code

--- remove_files.py
+++ remove_files.py
@@ -7,13 +7,14 @@ imgpath = "/Users/Fo/Dropbox/LearnPython/repo/images/little pics"
 
 for current, sub, files in os.walk(imgpath):
 	for file in files:
+		size = os.path.getsize(os.path.join(current,file))
+		print "Working with {} of size {}".format(file, size)
 		# check to see if they are JPGs
-		if file.lower(0).endswith(".jpg"):
+		if file.lower().endswith(".jpg"):
 			# check if they are smaller than 2KB
 			if os.path.getsize(os.path.join(current,file)) < 2000:
-				print "remove this file"
+				os.remove(os.path.join(current,file))
 			else:
 				print "we do nothing; it's too big"
 		else:
 			print "we do nothing; not a jpg"
-			
