dff11af402186bcba3b93c0776377676cdada428
Fo <chiapolinn@gmail.com>
2014-11-16 18:06:30 -0700

    Error handling

--- csv_parse.py
+++ csv_parse.py
@@ -1,6 +1,7 @@
 import argparse
 import csv
 import os
+import sys
 
 parser = argparse.ArgumentParser()
 #store input file name
@@ -8,9 +9,27 @@ parser.add_argument('-i', action='store')
 #store output file name
 parser.add_argument('-o', action='store')
 #store number of rows
-parser.add_argument('-r', action='store', type=int)
+parser.add_argument('-r', action='store')
 
 args = parser.parse_args()
+
+# check to see if input exists
+try: 
+	with open(args.i) as f:
+		pass
+except IOError:
+	print "Your input file does not exist"
+	sys.exit(0)
+
+# check to see if rows is an integer
+
+try:
+	rows = int(args.r)
+except ValueError:
+	print "You didn't input an integer"
+	sys.exit(0)
+
+
 
 print args.i
 print args.o
