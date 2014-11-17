b9cb26649cdccd9de56f93b53d6895221c752d83
Fo <chiapolinn@gmail.com>
2014-11-16 18:15:18 -0700

    Added checks for CSV row size

--- csv_parse.py
+++ csv_parse.py
@@ -3,6 +3,7 @@ import csv
 import os
 import sys
 
+# create a parser to handle command-line arguments
 parser = argparse.ArgumentParser()
 #store input file name
 parser.add_argument('-i', action='store')
@@ -13,21 +14,34 @@ parser.add_argument('-r', action='store')
 
 args = parser.parse_args()
 
-# check to see if input exists
-try: 
-	with open(args.i) as f:
-		pass
-except IOError:
-	print "Your input file does not exist"
-	sys.exit(0)
+total_rows = 0
+rows = 0
 
 # check to see if rows is an integer
-
 try:
 	rows = int(args.r)
 except ValueError:
+	# error handle and quit if does not eixst
 	print "You didn't input an integer"
 	sys.exit(0)
+
+# check to see if input exists
+try: 
+	with open(args.i) as file:
+		pass
+except IOError:
+	# error handle and quit if does not exist
+	print "Your input file does not exist"
+	sys.exit(0)
+
+# count total number of rows
+with open(args.i) as input:
+	total_rows = sum(1 for row in input)-1
+
+if total_rows <= rows:
+	print "The split number is more than or equal to the size of the CSV to split"
+	sys.exit(0)
+
 
 
 
