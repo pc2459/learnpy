5b87ee649418750c265a9b1f78e4694b69ab8e2f
Fo <chiapolinn@gmail.com>
2014-11-27 15:04:59 +0800

    Regex scraping

--- notes/day4.py
+++ notes/day4.py
@@ -40,3 +40,24 @@ start_index = text.find(start_tag) + len(start_tag)
 end_index = text.find(end_tag)
 
 print text[start_index:end_index]
+
+# Using Regex to do the same, allowing for weirdness in the tags
+
+import re
+
+my_address = "https://realpython.com/practice/dionysus.html"
+
+html_page = urlopen(my_address)
+html_text = html_page.read()
+# Python 3: html_text = html_page.read().decode('utf-8')
+
+match_results = re.search("<title .*?>.*</title .*?>", html_text,
+re.IGNORECASE)
+
+# I'll note that this is still not perfect - what if there is a 
+# space BEFORE the title tag, i.e. < title>?
+
+title = match_results.group()
+title = re.sub("<.*?>", "", title) # remove HTML tags
+
+print title
